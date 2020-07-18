a9da6399e233869c2eb7eaae8accab92d106e42a
Nortus222 <Nortus222@gmail.com>
2020-07-18 19:50:13 +0300

    Adding Commands

---
+++ BunkerBot2/Abstractions/TelegramCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+
+namespace BunkerBot2.Abstractions
+{
+    public abstract class TelegramCommand
+    {
+        public abstract string Name { get; }
+
+        public abstract Task Execute
+    }
+}
---
+++ BunkerBot2/Commands/AdminCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram;
+
+
+
+namespace BunkerBot2.Commands
+{
+    public class AdminCommand: TelegramCommand
+    {
+        public AdminCommand()
+        {
+        }
+    }
+}
