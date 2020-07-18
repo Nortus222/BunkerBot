73940a5f54a6646c9d557e8750b3634c703f0056
Anton <anton@pop-os.localdomain>
2020-07-18 18:44:44 +0300

    Added files

--- BunkerBot2/Program.cs
+++ BunkerBot2/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using static System.Console;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-namespace BunkerBot2
+namespace Bot1
 {
     class Program
     {
+        private static TelegramBotClient client;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            client = new TelegramBotClient("1111775546:AAEqesvfDbF-UDS48I1OtwAxNprAFvkJ_NI");
+            client.OnMessage += BotOnMessageReceived;
+            client.OnMessageEdited += BotOnMessageReceived;
+            //client.SendTextMessageAsync()
+            client.StartReceiving();
+            Console.ReadLine();
+            client.StopReceiving(); 
         }
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;         
+            if (message?.Type == MessageType.Text)
+            await client.SendTextMessageAsync(message.Chat.Id, "/start - start");
+            //string start = "/start";
+            {
+                // switch (message.Text)
+                // {
+                //     case start:
+                //     await client.SendTextMessageAsync(message.Chat.Id, "Welcome");
+                // }
+                if(message.Text == "/start")
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "Welcome");
+                }
+            }
+        } 
+                    
     }
 }
