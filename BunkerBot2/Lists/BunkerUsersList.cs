06f860a07cf55516d5266ee04ac3555753c150fa
Nortus222 <Nortus222@gmail.com>
2020-07-19 18:29:22 +0300

    Added new files

---
+++ BunkerBot2/Lists/BunkerUsersList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BunkerBot2.Lists
+{
+    public class BunkerUsersList
+    {
+
+        public List<BunkerUser> Users { get; }
+
+        public BunkerUsersList(List<BunkerUser> users)
+        {
+            Users = users;
+        }
+
+        public BunkerUser GetUserById(long id)
+        {
+            foreach (BunkerUser user in Users)
+            {
+                if (user.EqualID(id))
+                {
+                    return user;
+                }
+            }
+
+            return null;
+        }
+
+        public bool CheckExistance(BunkerUser user)
+        {
+            foreach (var us in Users)
+            {
+                if (us.Equals(user))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Add(BunkerUser user)
+        {
+            Users.Add(user);
+        }
+
+        public void Clear()
+        {
+            Users.Clear();
+        }
+
+        public void RemoveUser(BunkerUser user)
+        {
+            Users.Remove(user);
+        }
+
+    }
+}
---
+++ BunkerBot2/Lists/RoomsList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Bot1;
+using Telegram.Bot;
+
+namespace BunkerBot2.Lists
+{
+    public class RoomsList
+    {
+
+        public List<Room> Rooms { get; }
+
+        public RoomsList(List<Room> rooms)
+        {
+            Rooms = rooms;
+            
+        }
+
+        public bool CreateRoom(BunkerUser user)
+        {
+            bool hasRoom = false;
+            foreach (var room in Rooms)
+            {
+                if (room.Host == user)
+                {
+                    Program.SendMessage(user, "You already have a room. Use it or delete it.");
+                    hasRoom = true;
+                }
+            }
+            if (!hasRoom)
+            {
+                Rooms.Add(new Room(user));
+                Program.SendMessage(user, "The room has been created.");
+                return true;
+            }
+            return false;
+        }
+
+        public void DeleteRoom(BunkerUser user)
+        {
+            for (int i = 0; i < Rooms.Count; i++)
+            {
+                if (Rooms[i].Host == user) Rooms.Remove(Rooms[i]);
+                return;
+            }
+            Program.SendMessage(user, "You are not a host");
+        }
+    }
+}
