4206bda945a6e053b3c948e0b1135bebc4361668
Nortus222 <Nortus222@gmail.com>
2020-07-19 09:05:51 +0300

    Adding StartCommand

---
+++ BunkerBot2/Commands/StartCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using BunkerBot2.Abstractions;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BunkerBot2.Commands
+{
+    public class StartCommand: TelegramCommand
+    {
+        public StartCommand()
+        {
+        }
+
+        public override string Name { get; } = "/start";
+
+        public override bool Contains(Message message)
+        {
+            if (message.Type != MessageType.Text)
+            {
+                return false;
+            }
+           
+            return message.Text.Contains(Name);
+        }
+
+        public override async Task Execute(Message message, ITelegramBotClient client)
+        {
+            var keyboard = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup(
+                        new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardButton[][]
+                        {
+                            new[]
+                            {
+                                InlineKeyboardButton.WithCallbackData("/help"),
+                                InlineKeyboardButton.WithCallbackData("/start")
+
+                            },
+                            new[]
+                            {
+                                InlineKeyboardButton.WithCallbackData("/host")
+                            }
+                        }
+                    ) ;
+
+            await client.SendTextMessageAsync(message.Chat.Id, "Choose option", replyMarkup: keyboard);
+        }
+    }
+}
---
+++ BunkerBot2/Service/CommandService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BunkerBot2.Commands;
+using BunkerBot2.Abstractions;
+using System.Collections.Generic;
+
+namespace BunkerBot2.Service
+{
+    public class CommandService
+    {
+        private readonly List<TelegramCommand> commands;
+
+        public CommandService()
+        {
+            commands = new List<TelegramCommand>
+            {
+                new AdminCommand(),
+                new StartCommand()
+            };
+
+        }
+
+        public List<TelegramCommand> Get() => commands;
+
+    }
+}
