56f08fb26cae6451b467be7df8bf153fa8362077
Anton Podkur <59440452+antonpodkur@users.noreply.github.com>
2020-07-22 18:46:06 +0300

    remove 1 little bug in poll

--- BunkerBot2/Commands/VoteCommand.cs
+++ BunkerBot2/Commands/VoteCommand.cs
@@ -25,41 +25,47 @@ namespace BunkerBot2.Commands
             Room room = Program.rooms.GetRoomByPlayer(user);
 
             int size = room.Players.Count;
-
-            string[] options = new string[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                options[i] = $"{room.Players[i].NickName}";
-            }
-
-            var pollInt = await client.SendPollAsync(room.Players[0].ChatID, "Whom to quit", options);
-
-            for (int i = 1; i< size; i++)
-            {
-                await client.ForwardMessageAsync(room.Players[i].ChatID, pollInt.Chat.Id, pollInt.MessageId);
-            }
-
-            await Task.Delay(20000);
-
-            var stoppedPoll = await client.StopPollAsync(pollInt.Chat.Id, pollInt.MessageId);
-
-            int max = 0;
-
-            string name = "";
-
-            foreach(PollOption option in stoppedPoll.Options)
-            {
-                if(option.VoterCount > max)
-                {
-                    max = option.VoterCount;
-                    name = option.Text;
-                }
-            }
-
-            BunkerUser player = room.GetPlayerByName(name);
-            room.RemovePlayer(player);
             
+            if(size == 1)
+            {
+                Program.SendMessage(user,"You must have at least 2 players to start a poll!");
+            }
+            else
+            {
+                string[] options = new string[size];
+
+                for (int i = 0; i < size; i++)
+                {
+                    options[i] = $"{room.Players[i].NickName}";
+                }
+
+                var pollInt = await client.SendPollAsync(room.Players[0].ChatID, "Whom to quit", options);
+
+                for (int i = 1; i< size; i++)
+                {
+                    await client.ForwardMessageAsync(room.Players[i].ChatID, pollInt.Chat.Id, pollInt.MessageId);
+                }
+
+                await Task.Delay(20000);
+
+                var stoppedPoll = await client.StopPollAsync(pollInt.Chat.Id, pollInt.MessageId);
+
+                int max = 0;
+
+                string name = "";
+
+                foreach(PollOption option in stoppedPoll.Options)
+                {
+                    if(option.VoterCount > max)
+                    {
+                        max = option.VoterCount;
+                        name = option.Text;
+                    }
+                }
+
+                BunkerUser player = room.GetPlayerByName(name);
+                room.RemovePlayer(player);
+            }
 
             
         }
