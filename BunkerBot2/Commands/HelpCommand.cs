5898be17719dd20fca0425c9e282998ad3e63553
Anton Podkur <59440452+antonpodkur@users.noreply.github.com>
2020-07-22 18:37:01 +0300

    edited /help command

--- BunkerBot2/Commands/HelpCommand.cs
+++ BunkerBot2/Commands/HelpCommand.cs
@@ -18,7 +18,19 @@ namespace BunkerBot2.Commands
 
         public override async Task Execute(BunkerUser user, ITelegramBotClient client)
         {
-            string message = " /start - start a bot \n /host - to became a host of the game \n /join - to join the game \n /quit - deletes you from bot memory \n /end - deletes all users from bot memory ";
+            string message; //= " /start - start a bot \n /host - to became a host of the game \n /join - to join the game \n /quit - deletes you from bot memory \n /end - deletes all users from bot memory ";
+            if(user.IsHost)
+            {
+                message = " /players - show players in your room\n /vote - choose who to expel\n /quit - leave the room\n /end - delete all users from bot memory\n /gameover - close room\n /createcards - create cards for all players in the room";
+            }
+            else if(user.IsPlayer)
+            {
+                message = " /players - show players in your room\n /quit - leave the room\n /end - delete all users from bot memory";
+            }
+            else
+            {
+                message = " /end - delete all users from bot memory\n /host - create a room and become a host\n /join - join the room";
+            }
 
             await client.SendTextMessageAsync(user.ChatID, message);
         }
